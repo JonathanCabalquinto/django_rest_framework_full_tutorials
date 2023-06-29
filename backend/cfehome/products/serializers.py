5146c4076e4d81b4fc23275fa1fb32ed0d0cdf14
JonathanCabalquinto <narridon@gmail.com>
2023-06-29 15:18:53 +0800

    #refs-drf "URLs, Reverse, & Serializers"

--- backend/cfehome/products/serializers.py
+++ backend/cfehome/products/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from .models import Product
 
 
@@ -12,10 +13,35 @@ class ProductSerializer(serializers.ModelSerializer):
             return None
         return obj.get_discount()
 
+    # url = serializers.SerializerMethodField(read_only=True)
+
+    # Append URL to each product to view it
+    # def get_url(self, obj):
+    #     request = self.context.get('request')
+    #     if request is None:
+    #         return None
+    #     return reverse("product-detail", kwargs={'pk': obj.pk}, request=request)
+
+    # Shorter way
+    view_url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk'
+    )
+
+    edit_url = serializers.SerializerMethodField(read_only=True)
+
+    def get_edit_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={'pk': obj.pk}, request=request)
+
     class Meta:
         model = Product
         fields = [
-            'id',
+            'edit_url',
+            'view_url',
+            'pk',
             'title',
             'content',
             'price',
--- backend/cfehome/products/urls.py
+++ backend/cfehome/products/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.ProductDetailsAPIView.as_view()),
-    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()),
+    path('<int:pk>/', views.ProductDetailsAPIView.as_view(), name='product-detail'),
+    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view(), name='product-edit'),
     path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view()),
-    path('', views.ProductListCreatAPIView.as_view())
+    path('', views.ProductListCreatAPIView.as_view(), name='product-list')
 ]
